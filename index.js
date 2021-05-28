ec7e428c817047ea5f48eeb14b53e6a0b2255d16
Hyeokjin Kang <coupy@croissant.tech>
2021-05-28 18:17:54 +0900

    add:이제 mongodb와 연결합니다.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const app = require('express')();
+const config = require('./config/config.json');
+const MongoClient = require('mongodb').MongoClient;
+
+const client = new MongoClient(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const port = process.env.PORT || 80;
+
+client.connect(err => {
+  const collection = client.db(config.db.name).collection("users");
+  collection.find({}).toArray((err, docs) => {
+    console.log(err);
+    console.log(docs);
+    client.close();
+  });
+});
+
 
 app.get('/', (req, res) => {
   res.send("Hello, world!");
