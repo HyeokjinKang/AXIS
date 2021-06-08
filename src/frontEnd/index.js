320c7674bb6baf90cd4d56b43e328cecbcf1afa6
HyeokjinKang <coupy@croissant.tech>
2021-06-08 18:01:06 +0900

    front-end 서버 구축

---
+++ src/frontEnd/index.js
@@ -0,0 +1,25 @@
+//config
+const config = require('../../config/config.json');
+
+//libraries
+const express = require('express');
+
+//express environment
+const port = 1025;
+const app = express();
+
+//express settings
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/../../views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/../../public"));
+
+app.get('/', (req, res) => {
+  res.send("hi");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
