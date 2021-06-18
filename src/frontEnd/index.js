80536198b9c7412b9891d5ecffb7608078b6f51e
HyeokjinKang <coupy@croissant.tech>
2021-06-19 00:36:54 +0900

    add: 언젠가 만들 관리자페이지 추가

--- src/frontEnd/index.js
+++ src/frontEnd/index.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../../public"));
 
-app.get('/', (req, res) => {
-  res.send("hi");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(port, () => {
